fd373c566cb7b8b43cf8d8043aec4407cf3ddcb8
Adam Doe <ADoe@mic.lan>
2016-05-11 11:47:47 -0400

    Delete wp_options added

--- uninstall.php
+++ uninstall.php
@@ -5,6 +5,17 @@
  * @package    addGTM
  */
 // If uninstall not called from WordPress, then exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
 }
+
+$option_name = 'gtm_script';
+
+delete_option( $option_name );
+
+// For site options in Multisite
+delete_site_option( $option_name );
+
+// Delete from db
+global $wpdb;
+$wpdb->query( "DELETE FROM wp_options WHERE option_name= $option_name;" );
